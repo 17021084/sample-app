513c6e936bb83254bc3c456d42d6c01068bc3dd6
Dang Vu Nhat Quang <dang.vu.nhat.quang@sun-asterisk.com>
2020-07-23 15:55:01 +0700

    Chapter 7 - Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,34 @@
 class UsersController < ApplicationController
-  def new; end
+  before_action :find_user, only: :show
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t(".flash", username: @user.name)
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
+  end
+
+  def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+    return if @user
+
+    flash[:danger] = t ".flash"
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # Routes define
   scope "(:locale)", locale: /en|jp/ do
     root "static_pages#home"
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
+    resources :users, except: :new
   end
 end
