3268e94836de127e09000ccb46068301180935d5
Dang Vu Nhat Quang <dang.vu.nhat.quang@sun-asterisk.com>
2020-07-27 08:31:51 +0700

    Chapter 3 - Mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Routes define
+  scope "(:locale)", locale: /en|jp/ do
+    root "static_pages#home"
+    get "/help", to: "static_pages#help"
+    get "/about", to: "static_pages#about"
+    get "/contact", to: "static_pages#contact"
+  end
 end
