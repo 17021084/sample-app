4dda7183f6fc7fb3746efce325d2ddd0989a7614
Dang Vu Nhat Quang <dang.vu.nhat.quang@sun-asterisk.com>
2020-07-31 13:09:28 +0700

    Chapter 10 - Updating, showing and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  before_action :find_user, :require_user, only: :show
+  before_action :find_user, except: %i(index new create)
+  before_action :require_logged_in, except: %i(new create)
+  before_action :require_same_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.page(params[:page]).per Settings.pagination.per_page
+  end
 
   def new
     @user = User.new
@@ -10,14 +17,31 @@ class UsersController < ApplicationController
 
     if @user.save
       log_in @user
-      flash[:success] = t(".flash", username: @user.name)
-      redirect_to user_path(@user)
+      flash[:success] = t ".flash", username: @user.name
+      redirect_to user_path @user
     else
-      render "new"
+      render :new
     end
   end
 
   def show; end
+
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t ".success"
+      redirect_to user_path locale, @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t ".success"
+    redirect_to users_path locale
+  end
 
   private
 
@@ -31,5 +55,25 @@ class UsersController < ApplicationController
 
     flash[:danger] = t ".flash"
     redirect_to root_path
+  end
+
+  def require_logged_in
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t ".unauthorized"
+    redirect_to login_path locale
+  end
+
+  def require_same_user
+    @user = User.find_by id: params[:id]
+    return if current_user? @user
+
+    flash[:danger] = t ".forbidden"
+    redirect_to root_path
+  end
+
+  def admin_user
+    redirect_to root_path locale unless current_user.admin?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_092944) do
+ActiveRecord::Schema.define(version: 2020_08_03_014624) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_092944) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
